a33d4114ce4152d9fb5d891f421445e0019b6846
JinHyukKo <112324054+JinhyukKo@users.noreply.github.com>
2026-01-18 12:06:15 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+const toggle = document.getElementById("themeToggle");
+
+toggle.addEventListener("click", () => {
+  document.body.classList.toggle("light");
+
+  localStorage.setItem(
+    "theme",
+    document.body.classList.contains("light") ? "light" : "dark"
+  );
+});
+
+const saved = localStorage.getItem("theme");
+if (saved === "light") {
+  document.body.classList.add("light");
+}
